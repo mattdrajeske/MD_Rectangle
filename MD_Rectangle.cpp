1d7954c2b717853bb2d60a59727a9e2f6346f9e1
Matt <drajeskematt@gmail.com>
2018-11-13 22:14:30 -0600

    add loop

--- MD_Rectangle.cpp
+++ MD_Rectangle.cpp
@@ -4,14 +4,37 @@
 #include "stdafx.h"
 #include "Rectangle.h"
 #include <iostream>
-//#include "Rectangle.cpp"
+#include <string>
 
+using namespace std;
+
+//bool equals(string, string);
 
 int main()
 {
-	Rectangle rect;
-	rect.setData();
-	rect.showData();
-    return 0;
+	string cont;
+	string test = "yes";
+	bool stop = false;
+
+	while(!stop){ //loops until stop condition
+		Rectangle rect;
+		rect.setData();
+		rect.showData();
+
+		cout << "Enter yes to continue: ";
+		cin >> cont;
+		for (int i = 0; i < cont.length(); i++) {
+			if (isupper(cont[i])) {        //checks if the string the user put in equals yes with any capitalization
+				cont[i] = tolower(cont[i]);
+			}
+			if (cont[i] != test[i]) { //user did not input yes, and therefore does not want to continue program
+				stop = true;		  //need variable so that outer loop will also stop
+				break;
+			}
+		}
+		//cout << cont << endl;
+	}
+	return 0;
 }
+
 
